2426d8e1780882e2c5dd7d1d62b384057b2fc676
José Augusto <ze.ccenci@gmail.com>
2022-03-10 19:43:50 -0300

    Criando Models e Configurando o JWT no json WEP API

---
+++ CleanArchMvc.WebApi/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebApi.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max " +
+            "{1} characters long. ", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
